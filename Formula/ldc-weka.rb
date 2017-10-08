df77d27969d9cd0af9f2d11b0c377ed2761ab1b2
Ori Peleg <nospam@orip.org>
2017-10-08 11:20:44 +0300

    added --HEAD support for weka-master (supports LLVM 5.0)
    Also re-synced with upstream https://github.com/Homebrew/homebrew-core/blob/master/Formula/ldc.rb

--- Formula/ldc-weka.rb
+++ Formula/ldc-weka.rb
@@ -4,7 +4,7 @@ class LdcWeka < Formula
   version_scheme 1
 
   stable do
-    url "https://github.com/weka-io/ldc.git", :shallow => false, :revision => "d1d424f5452ecd28ac5edcb0c2079bc8f56d652c"
+    url "https://github.com/weka-io/ldc.git", :shallow => false, :revision => "c70b2c1f551ceec7c028e01576a0d3694fa1dce9"
     version "1.1.0-mac6"
 
     resource "ldc-lts" do
@@ -12,12 +12,21 @@ class LdcWeka < Formula
       sha256 "7aa540a135f9fa1ee9722cad73100a8f3600a07f9a11d199d8be68887cc90008"
     end
 
-    depends_on "libconfig"
+  end
+
+  head do
+    url "https://github.com/weka-io/ldc.git", :shallow => false, :branch => "weka-master"
+
+    resource "ldc-lts" do
+      url "https://github.com/ldc-developers/ldc/releases/download/v0.17.5/ldc-0.17.5-src.tar.gz"
+      sha256 "7aa540a135f9fa1ee9722cad73100a8f3600a07f9a11d199d8be68887cc90008"
+    end
   end
 
   needs :cxx11
 
   depends_on "cmake" => :build
+  depends_on "libconfig" => :build
   depends_on "llvm"
 
   conflicts_with "ldc", :because => "this is a patched ldc"
@@ -25,6 +34,7 @@ class LdcWeka < Formula
   def install
     ENV.cxx11
     (buildpath/"ldc-lts").install resource("ldc-lts")
+
     cd "ldc-lts" do
       mkdir "build" do
         args = std_cmake_args + %W[
@@ -39,7 +49,11 @@ class LdcWeka < Formula
         -DLLVM_ROOT_DIR=#{Formula["llvm"].opt_prefix}
         -DINCLUDE_INSTALL_DIR=#{include}/dlang/ldc
         -DD_COMPILER=#{buildpath}/ldc-lts/build/bin/ldmd2
+        -DLDC_WITH_LLD=OFF
+        -DRT_ARCHIVE_WITH_LDC=OFF
       ]
+      # LDC_WITH_LLD see https://github.com/ldc-developers/ldc/releases/tag/v1.4.0 Known issues
+      # RT_ARCHIVE_WITH_LDC see https://github.com/ldc-developers/ldc/issues/2350
 
       system "cmake", "..", *args
       system "make"
@@ -54,8 +68,11 @@ class LdcWeka < Formula
         writeln("Hello, world!");
       }
     EOS
-
     system bin/"ldc2", "test.d"
+    assert_match "Hello, world!", shell_output("./test")
+    system bin/"ldc2", "-flto=thin", "test.d"
+    assert_match "Hello, world!", shell_output("./test")
+    system bin/"ldc2", "-flto=full", "test.d"
     assert_match "Hello, world!", shell_output("./test")
     system bin/"ldmd2", "test.d"
     assert_match "Hello, world!", shell_output("./test")
