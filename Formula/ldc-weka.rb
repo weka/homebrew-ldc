e0202f4d7410fb9293f8761d06f3eabf02f171cf
Ori Peleg <orip@weka.com>
2021-05-06 17:15:18 +0300

    synchronized with upstream ldc formula + bumped to v1.24.0-weka3
    - Based on v1.24.0 formula (acdfed7668f19cc7c538f993f2ec06f2fcadc9f1 in homebrew-core)
    - Explicitly require llvm 11

--- Formula/ldc-weka.rb
+++ Formula/ldc-weka.rb
@@ -1,24 +1,28 @@
 class LdcWeka < Formula
   desc "Portable D programming language compiler - fork for Weka.IO"
   homepage "https://wiki.dlang.org/LDC"
-  url "https://github.com/weka-io/ldc/releases/download/v1.21.0-weka/ldc-weka-1.21.0-weka.tar.xz"
-  version "1.21.0-weka"
-  sha256 "d86859dc04fddb6b9917338b1fcdb8bbc7258249dcab02691f2fa5401cbd0392"
+  url "https://github.com/weka/ldc/releases/download/v1.24.0-weka3/ldc-weka-1.24.0-weka3.tar.xz"
+  version "1.24.0-weka3"
+  sha256 "ad610241f71ac1d7782eaabeff652c9f0d92e35c11301db026e6dda9b175ce44"
   license "BSD-3-Clause"
   head "https://github.com/weka-io/ldc.git", :shallow => false, :branch => "weka-master"
 
   conflicts_with "ldc", :because => "this is a patched ldc"
   version_scheme 2
 
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
+
   bottle do
-    root_url "https://github.com/weka-io/ldc/releases/download/v1.21.0-weka"
-    sha256 catalina: "d4b9d31de8adb52ee1e7d6a5296ce8ff223b8c45fc9c74636b0adba44bc67dd1"
-    sha256 big_sur: "6a16a2014117fb1b6ba144e5c1031540fe114afbe8128b8f49fbe03fda56455d"
+    root_url "https://github.com/weka/ldc/releases/download/v1.24.0-weka3"
+    sha256 big_sur: "ee7a4072a97f693a3a6f0cafc42463e5b2528ef725d20f31102e4cf3f1ebd68a"
   end
 
   depends_on "cmake" => :build
   depends_on "libconfig" => :build
-  depends_on "llvm@9" # due to a bug in llvm 10 https://bugs.llvm.org/show_bug.cgi?id=47226
+  depends_on "llvm@11"
 
   uses_from_macos "libxml2" => :build
 
@@ -27,10 +31,14 @@ class LdcWeka < Formula
   end
 
   resource "ldc-bootstrap" do
-    url "https://github.com/ldc-developers/ldc/releases/download/v1.23.0/ldc2-1.23.0-osx-x86_64.tar.xz"
-    version "1.23.0"
-    sha256 "b3a6ec50f83063a66d5d538c635b1d1efc454bd8f2f8d74adaa93c36e1566dab"
+    url "https://github.com/ldc-developers/ldc/releases/download/v1.24.0/ldc2-1.24.0-osx-x86_64.tar.xz"
+    sha256 "91b74856982d4d5ede6e026f24e33887d931db11b286630554fc2ad0438cda44"
   end
+
+  # Add support for building against LLVM 11.1
+  # This is already merged upstream via https://github.com/ldc-developers/druntime/pull/195
+  # but it needs adjustments to apply against 1.24.0 tarball
+  patch :DATA
 
   def install
     ENV.cxx11
@@ -38,7 +46,7 @@ class LdcWeka < Formula
 
     mkdir "build" do
       args = std_cmake_args + %W[
-        -DLLVM_ROOT_DIR=#{Formula["llvm@9"].opt_prefix}
+        -DLLVM_ROOT_DIR=#{Formula["llvm@11"].opt_prefix}
         -DINCLUDE_INSTALL_DIR=#{include}/dlang/ldc
         -DD_COMPILER=#{buildpath}/ldc-bootstrap/bin/ldmd2
       ]
@@ -46,6 +54,9 @@ class LdcWeka < Formula
       system "cmake", "..", *args
       system "make"
       system "make", "install"
+
+      # Workaround for https://github.com/ldc-developers/ldc/issues/3670
+      cp Formula["llvm@11"].opt_lib/"libLLVM.dylib", lib/"libLLVM.dylib"
     end
   end
 
@@ -66,3 +77,15 @@ class LdcWeka < Formula
     assert_match "Hello, world!", shell_output("./test")
   end
 end
+
+__END__
+--- ldc-1.24.0-src/runtime/druntime/src/ldc/intrinsics.di.ORIG	2021-02-19 00:16:52.000000000 +0000
++++ ldc-1.24.0-src/runtime/druntime/src/ldc/intrinsics.di	2021-02-19 00:17:05.000000000 +0000
+@@ -26,6 +26,7 @@
+ else version (LDC_LLVM_900)  enum LLVM_version =  900;
+ else version (LDC_LLVM_1000) enum LLVM_version = 1000;
+ else version (LDC_LLVM_1100) enum LLVM_version = 1100;
++else version (LDC_LLVM_1101) enum LLVM_version = 1101;
+ else static assert(false, "LDC LLVM version not supported");
+ 
+ enum LLVM_atleast(int major) = (LLVM_version >= major * 100);
